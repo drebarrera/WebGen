b638001aa877125323503c5a3264fa9b1cf0970f
Andres Barrera <drebarrera@yahoo.com>
2022-04-10 09:05:05 -0400

    Update for Inadmissible Array

--- modules.py
+++ modules.py
@@ -1,5 +1,5 @@
 import sys
-inadmissible = ["type", "id", "cl", "onclick", "onhover", "target", "content", "name", "rows", "cols", "font_size", "height", "width", "margin", "margin_left", "margin_right", "margin_top", "margin_bottom", "src", "autoplay", "muted", "controls", "loop"]
+inadmissible = ["type", "id", "cl", "onclick", "onhover", "target", "content", "name", "rows", "cols", "font_size", "height", "width", "margin", "margin_left", "margin_right", "margin_top", "margin_bottom", "src", "autoplay", "muted", "controls", "loop","custom"]
 dynamic = ["font_size", "height", "width", "margin", "margin_left", "margin_right", "margin_top", "margin_bottom"]
 
 # Data -> Webpage Header Data
